03f8b7142abccfd420433d92ff12ea31d9cc78c0
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-07-10 16:11:07 -0300

    Update

---
+++ JS/Aula03/dom.js
@@ -0,0 +1,15 @@
+/*
+DOM
+    Só 1 elemento:
+        getElementById()
+
+    Mais de 1 elemento:
+        getElementsByTagName()
+        getElementsByClassName()
+        getElementsByName()
+*/
+
+console.log(document.getElementById("nome_usuario")) // Elemento específico
+console.log(document.getElementsByTagName("input")) // Coleção
+console.log(document.getElementsByClassName("campo_texto")) // Coleção
+console.log(document.getElementsByName("nome")) // NodeLiist
