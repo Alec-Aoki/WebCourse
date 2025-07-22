2d421197bdfd9aaaa3668ca3a5aaaef3a8519405
Alec Campos Aoki <alec.camposaoki@gmail.com>
2025-07-22 17:25:28 -0300

    Update

---
+++ JS/Auila10/objetos.js
@@ -0,0 +1,92 @@
+/* Objetos Literais/Estáticos */
+// São objetos únicos, ou seja, não podem ser copiados
+// Se atribuirmos um objeto a uma variável, a variável
+// irá apontar para esse objeto, não copiá-lo
+
+let objetoLiteral = {
+    // Conjuntos/pares de nome/chave/atributo e valor
+    descricao: 'Notebook',
+    preco: 1500,
+    verDescricao: function(){
+        console.log(`${this.descricao} por apenas ${this.preco}`)
+    }
+}
+
+objetoLiteral.verDescricao()
+
+/* Outras notações */
+let nome = 'Jorge'
+let idade = 29
+let sexo = 'Masculino'
+let profissao = 'Programador'
+
+let objeto = {
+    nome: nome,
+    idade: idade,
+    sexo: sexo,
+    profissao: profissao,
+    exibirResumo: function(){
+        console.log(`${this.nome} tem ${this.idade} anos, é do sexo ${this.sexo} e é ${this.profissao}`)
+    }
+}
+
+objeto.exibirResumo()
+
+let objeto2 = {
+    // nome = nome da variável, valor = valor da variável
+    nome,
+    idade,
+    sexo,
+    profissao,
+    exibirResumo(){
+        console.log(`${this.nome} tem ${this.idade} anos, é do sexo ${this.sexo} e é ${this.profissao}`)
+    }
+}
+
+objeto2.nome = 'Roberto' // Alterando valor
+objeto2.exibirResumo()
+
+/* Inclusão dinâmica de atributos */
+objeto2.gostos = 'Café'
+objeto2.revelarGostos = () => console.log(`${objeto2.nome} gosta de ${objeto2.gostos}`)
+
+objeto2.revelarGostos()
+
+let objeto3 = objeto2
+objeto3.nome = 'Alec'
+objeto2.exibirResumo()
+
+/* Funções Construtoras */
+// Permite criar várias instâncias de um objeto,
+// além de permitir variáveis privadas
+
+let Carro = function(){
+    var velocidadeAtual = 0
+    var velocidadeMaxima = 180
+
+    this.cor = 'Amarelo'
+    this.modelo = 'Monza'
+
+    this.acelerar = function(){
+        let velocidadeFinal = velocidadeAtual + 10
+        if(velocidadeFinal < velocidadeMaxima){
+            velocidadeAtual += 10
+        }
+    }
+
+    this.getVelocidadeAtual = function(){
+        return velocidadeAtual
+    }
+
+    this.setVelocidadeAtual = function(novaVelocidade){
+        if(novaVelocidade < velocidadeMaxima && novaVelocidade >= 0){
+            velocidadeAtual = novaVelocidade
+        }
+    }
+}
+
+let carro = new Carro()
+carro.acelerar()
+console.log(`A velocidade atual do carro (${carro.modelo} ${carro.cor}) é: ${carro.getVelocidadeAtual()} km/h`)
+console.log(carro.velocidadeAtual) // undefined
+console.log(carro.getVelocidadeAtual()) // 10
--- JS/Aula09/POO.js
+++ JS/Aula09/POO.js
@@ -29,3 +29,119 @@ contaAlec.depositar(5)
 console.log(contaAlec.consultar_saldo())
 contaAlec.sacar(15)
 console.log(contaAlec.consultar_saldo())
+
+console.log('\n')
+
+/* Visibiliade */
+// JS não suporta visibilidade de OO, então a convenção
+// é usar um _ antes do nome do atributo caso ele for
+// ser considerado como privado
+class Tv{
+    constructor(){
+        this._canais = Array(1, 2, 3, 4, 5)
+        this._canalAtivo = 5
+        this._volume = 50
+    }
+
+    // Pseudovariáveis
+    get canalAtivo(){
+        return this._canalAtivo
+    }
+
+    set canalAtivo(canal){
+        if(this._canais.indexOf(canal) != -1){
+            this._canalAtivo = canal
+        } else {
+            console.log("Canal não encontrado!")
+        }
+    }
+}
+
+let tv = new Tv()
+console.log(tv.canalAtivo)
+
+tv.canalAtivo = 7
+console.log(tv.canalAtivo)
+
+tv.canalAtivo = 1
+console.log(tv.canalAtivo)
+
+/* Herança */
+class Animal {
+    constructor() {
+        this.cor = ''
+        this.tamanho = 10
+    }
+
+    dormir() {
+        console.log('Dormindo')
+    }
+}
+
+class Cachorro extends Animal {
+    constructor() {
+        super()
+        this.orelhas = 'Grandes e caídas'
+    }
+
+    latir(){
+        console.log("Latido")
+    }
+}
+
+class Passaro extends Animal {
+    constructor(bico) {
+        super()
+        this.bico = bico
+    }
+
+    voar(){
+        console.log("Voo")
+    }
+}
+
+let cachorro = new Cachorro()
+let passaro = new Passaro()
+
+console.log(cachorro)
+console.log(passaro)
+
+cachorro.dormir()
+console.log('\n')
+passaro.dormir()
+
+class Papagaio extends Passaro{
+    constructor(sabeFalar = false, bico){
+        super(bico)
+        this.sabeFalar = sabeFalar
+    }
+
+    falar(){
+        if(this.sabeFalar){
+            console.log("Falou")
+        } else {
+            console.log("Não sabe falar")
+        }
+    }
+}
+
+let papagaio = new Papagaio(true, 'pequeno')
+papagaio.falar()
+
+class Avestruz extends Passaro{
+    constructor(bico){
+        super(bico)
+    }
+
+    enterrarCabeca(){
+        console.log("Enterrou a cabeca")
+    }
+
+    // Polimorfismo -> sobrescreve métodos do pai
+    voar(){
+        console.log("Nao sabe voar")
+    }
+}
+
+let avestruz = new Avestruz('grande')
+avestruz.voar()
